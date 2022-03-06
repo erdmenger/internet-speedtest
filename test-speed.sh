6207c5fc562970355f209d2c9083c8d313eac408
Joerg Erdmenger <joerg.erdmenger@gmail.com>
2022-03-06 17:47:05 +0100

    with output dir

--- test-speed.sh
+++ test-speed.sh
@@ -3,14 +3,19 @@
 set -e
 # Script bricht bei einem Fehler ab
 
-if [ ! -e "speedtest.csv" ]; then
-    echo "Erstelle CSV-Datei mit CSV-Header"
-    speedtest-cli --csv-header > speedtest.csv
+if [ ! -e "output" ]; then
+    echo "Erstelle Ordner 'output'."
+    mkdir output
 fi
 
-if [ ! -e "speedtest-secure.csv" ]; then
+if [ ! -e "output/speedtest.csv" ]; then
     echo "Erstelle CSV-Datei mit CSV-Header"
-    speedtest-cli --csv-header --secure > speedtest-secure.csv
+    speedtest-cli --csv-header > output/speedtest.csv
+fi
+
+if [ ! -e "output/speedtest-secure.csv" ]; then
+    echo "Erstelle CSV-Datei mit CSV-Header"
+    speedtest-cli --csv-header --secure > output/speedtest-secure.csv
 fi
 
 while true; do
@@ -21,8 +26,11 @@ while true; do
     # in der Konsole aus.
     # damit der Header nicht verloren geht, nutze ich den
     # Optionsschalter -a, steht für append (anhängen)
-    speedtest-cli --csv | tee -a speedtest.csv
-    speedtest-cli --csv --secure | tee -a speedtest-secure.csv
+    speedtest-cli --csv | tee -a output/speedtest.csv
+    speedtest-cli --csv --secure | tee -a output/speedtest-secure.csv
+
+    # Gleich die Grafik mit gnuplot erzeuten lassen.
+    gnuplot < speedtest.gpi > output/speedtest.png
 
     # Test jede Stunde.
     sleep 3600
